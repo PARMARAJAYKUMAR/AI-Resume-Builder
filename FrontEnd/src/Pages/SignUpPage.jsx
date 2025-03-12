70031a94a03d84de420ef4a931747f123008e412
PARMARAJAYKUMAR <parmarajay0865@gmail.com>
2025-03-12 12:51:30 +0530

    Update SignUpPage.jsx

--- FrontEnd/src/Pages/SignUpPage.jsx
+++ FrontEnd/src/Pages/SignUpPage.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 function SignUpPage() {
-  return (
-    <div>
-      SignUpPage Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-      veritatis omnis officiis temporibus asperiores maiores quod iure.
-      Distinctio minima sit quia, totam unde, velit vero nesciunt nam sunt
-      incidunt magnam!
-    </div>
-  );
+  return <div>SignUpPage</div>;
 }
 
 export default SignUpPage;
